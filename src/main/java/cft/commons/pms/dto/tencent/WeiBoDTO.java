ea07bb073acf6b9f693021a045feab95405e25be
taurus <xxh0755@gmail.com>
2014-04-16 17:35:17 +0800

    新增DTO
    Signed-off-by: taurus <xxh0755@gmail.com>

--- src/main/java/cft/commons/pms/dto/tencent/WeiBoDTO.java
+++ src/main/java/cft/commons/pms/dto/tencent/WeiBoDTO.java
@@ -1,18 +1,27 @@
 package cft.commons.pms.dto.tencent;
 
 public class WeiBoDTO {
-	private Long id;// 微博唯一id
+	private String id;// 微博唯一id
 	private String text;// 微博内容
 	private String orgintext;// 微博原始内容
 	private String nick;// 发表者昵称
 	private String head;// 发表者头像url
-	private Long timetamp;// 发表时间
+	private int type;//微博类型,1-原创发表，2-转载，3-私信，4-回复，5-空回，6-提及，7-评论
+	private int timestamp;// 发表时间
 
-	public Long getId() {
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+	
+	public String getId() {
 		return id;
 	}
 
-	public void setId(Long id) {
+	public void setId(String id) {
 		this.id = id;
 	}
 
@@ -48,11 +57,11 @@ public class WeiBoDTO {
 		this.head = head;
 	}
 
-	public Long getTimetamp() {
-		return timetamp;
+	public int getTimestamp() {
+		return timestamp;
 	}
 
-	public void setTimetamp(Long timetamp) {
-		this.timetamp = timetamp;
+	public void setTimestamp(int timestamp) {
+		this.timestamp = timestamp;
 	}
 }
