b23c5017bce7f166cb665d59774e54df11924da0
taurus <xxh0755@gmail.com>
2014-04-16 17:35:00 +0800

    在TencentWeiBoXController类里面新增调用API的方法
    Signed-off-by: taurus <xxh0755@gmail.com>

--- src/main/java/cft/commons/pms/web/tencent/ViewController.java
+++ src/main/java/cft/commons/pms/web/tencent/ViewController.java
@@ -1,6 +1,9 @@
 package cft.commons.pms.web.tencent;
 
+import java.net.UnknownHostException;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,5 +12,13 @@ public class ViewController {
 	@RequestMapping("addWeiBoView.do")
 	public String addWeiBo(){
 		return "tencent/sendWeiBo";
+	}
+	
+	
+	@RequestMapping("addPicWeiBoView.do")
+	public String addPicWeiBo(Model model) throws UnknownHostException{
+		model.addAttribute("clientip", IPUtils.getClientIP());
+		
+		return "tencent/sendPicWeiBo";
 	}
 }
