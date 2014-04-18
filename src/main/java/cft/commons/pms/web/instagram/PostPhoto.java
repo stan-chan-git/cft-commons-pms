70483519e645d6ea2188368d940b4b27e7ad530d
user <user@10.211.55.3>
2014-04-18 09:07:59 +0800

    instagramController

---
+++ src/main/java/cft/commons/pms/web/instagram/PostPhoto.java
@@ -0,0 +1,35 @@
+package cft.commons.pms.web.instagram;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import cft.commons.core.util.HttpClientUtils;
+
+@Controller
+public class PostPhoto {
+    
+	/*@RequestMapping(value="create.do")
+	public String Create() throws IOException{
+		
+		
+		// 创建一个订阅
+		
+		 String
+		  subscriptionUrl="https://api.instagram.com/v1/subscriptions?client_id="
+		  +APP_KEY+ "&client_secret=" + CLIENT_SECET+"&object=user"
+		 +"&aspect=media"
+		  +"&verify_token=myVerifyToken"+"&callback_url="+REDIRECT_URL;
+		  
+		  String result2= HttpClientUtils.httpGet(subscriptionUrl, 3000, 9000);
+		
+		
+		return null;
+		
+		
+	}
+	*/
+	
+	
+}
