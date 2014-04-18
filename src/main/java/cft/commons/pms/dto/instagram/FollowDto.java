c7b0d3c2d36d036f9774f04ff049e7420ce4d9e9
user <user@10.211.55.3>
2014-04-18 09:07:41 +0800

    instagramDto
    Signed-off-by: user <user@10.211.55.3>

---
+++ src/main/java/cft/commons/pms/dto/instagram/FollowDto.java
@@ -0,0 +1,24 @@
+package cft.commons.pms.dto.instagram;
+
+public class FollowDto {
+    
+	private String username;
+	private String photo;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	
+	
+	
+	
+}
