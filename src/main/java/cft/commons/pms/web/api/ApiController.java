1d5b169ba4bd1d67c1add551fd1c218428db6f8d
taurus <xxh0755@gmail.com>
2014-04-21 13:45:04 +0800

    编写调用发微博API的controller
    Signed-off-by: taurus <xxh0755@gmail.com>

--- src/main/java/cft/commons/pms/web/api/ApiController.java
+++ src/main/java/cft/commons/pms/web/api/ApiController.java
@@ -1,8 +1,34 @@
 package cft.commons.pms.web.api;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import cft.commons.pms.service.api.TencentService;
 
 @Controller
 public class ApiController {
-
+	@Autowired
+	private TencentService tencentService;
+	private String tencent_info = "";
+	
+	//发表一条新消息(即：发表一条微博)
+	public String sendMessage(String content,HttpServletRequest request){
+		String access_token = (String)request.getSession().getAttribute("tencent_token");
+		String openid = (String)request.getSession().getAttribute("openid");
+		String openkey = (String)request.getSession().getAttribute("openkey");
+		
+		try {
+			tencent_info = tencentService.sendWeiBo(access_token, content,openid ,openkey );
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		if(tencent_info.equals("")){
+			request.setAttribute("fail_info", "腾讯微博发布失败!");
+		}
+		
+		return "";
+	}
 }
