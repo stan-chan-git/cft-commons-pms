ba7f6dbba7d73000b99d4ca464b42ee5b0b08256
taurus <xxh0755@gmail.com>
2014-04-21 14:30:23 +0800

    修改ApiController
    Signed-off-by: taurus <xxh0755@gmail.com>

--- src/main/java/cft/commons/pms/web/api/ApiController.java
+++ src/main/java/cft/commons/pms/web/api/ApiController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import cft.commons.pms.service.api.TencentService;
 
@@ -14,11 +15,12 @@ public class ApiController {
 	private String tencent_info = "";
 	
 	//发表一条新消息(即：发表一条微博)
+	@RequestMapping(value="sendMessage.do")
 	public String sendMessage(String content,HttpServletRequest request){
 		String access_token = (String)request.getSession().getAttribute("tencent_token");
 		String openid = (String)request.getSession().getAttribute("openid");
 		String openkey = (String)request.getSession().getAttribute("openkey");
-		
+System.out.println(content);		
 		try {
 			tencent_info = tencentService.sendWeiBo(access_token, content,openid ,openkey );
 		} catch (Exception e) {
@@ -29,6 +31,6 @@ public class ApiController {
 			request.setAttribute("fail_info", "腾讯微博发布失败!");
 		}
 		
-		return "";
+		return "api/result";
 	}
 }
