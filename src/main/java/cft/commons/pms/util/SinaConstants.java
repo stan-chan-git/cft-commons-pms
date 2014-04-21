cdd2ce5dfc3c054c2e80bb73b508c445ebcb35e0
luffy <lyhjob0514@gmail.com>
2014-04-21 14:07:20 +0800

    新浪constants文件和service
    Signed-off-by: luffy <lyhjob0514@gmail.com>

---
+++ src/main/java/cft/commons/pms/util/SinaConstants.java
@@ -0,0 +1,17 @@
+package cft.commons.pms.util;
+/**
+ * @author luffy
+ *
+ */
+public class SinaConstants {
+	
+	public static final String APP_KEY = "4281626272";
+	public static final String CLIENT_SECET = "69eade123bb72a88abadcdde95e8e6ae";
+	public static final String REDIRECT_URL = "http://localhost:8088/pms/sina/sinaweibo.do";
+	
+	/**
+	 * @sina api url
+	 *
+	 */
+	public static final String StatusesUpdateUrl = "https://api.weibo.com/2/statuses/update.json";
+}
