6feb26aa2bf1bd13adaf99cf58be2328fa66e092
taurus <xxh0755@gmail.com>
2014-04-22 13:28:06 +0800

    增加发带图片微博的调用函数
    Signed-off-by: taurus <xxh0755@gmail.com>

--- src/main/webapp/static/api/tencent.js
+++ src/main/webapp/static/api/tencent.js
@@ -18,3 +18,25 @@ function sendTencentWeiBo(content,callback){
 			//返回数据类型
 			"json");
 }
+
+//发一条带图片的微博
+function sendTencentPicWeiBo(content,callback){
+	$.post("/pms/tencent/sendPicWeiBo.do",
+			
+			//参数
+			{
+		      content:content
+			},
+			
+			//回调函数
+			function(data){
+				if(typeof(callback) != "undefined"){
+					callback("腾讯微博发布成功!");
+				}else{
+					callback("腾讯微博发布失败!");
+				}
+			},
+			
+		    //返回的数据类型
+	        "json");
+}
