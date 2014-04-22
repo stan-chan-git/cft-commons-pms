e30336d51e43d245aa65f6f7e77afdd6f4492554
luffy <lyhjob0514@gmail.com>
2014-04-22 17:38:21 +0800

    获取好友动态
    Signed-off-by: luffy <lyhjob0514@gmail.com>

--- src/main/java/cft/commons/pms/dto/sina/SinaDTO.java
+++ src/main/java/cft/commons/pms/dto/sina/SinaDTO.java
@@ -1,5 +1,8 @@
 package cft.commons.pms.dto.sina;
 
+import lombok.Data;
+
+@Data
 public class SinaDTO {
 
 	private Long id;//微博ID
@@ -9,41 +12,8 @@ public class SinaDTO {
 	private String screen_name;// 微博作者的用户昵称
 	private String created_at;// 微博创建时间
 	
-	public String getUserId() {
-		return userId;
-	}
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getText() {
-		return text;
-	}
-	public void setText(String text) {
-		this.text = text;
-	}
-	public String getSource() {
-		return source;
-	}
-	public void setSource(String source) {
-		this.source = source;
-	}
-	public String getScreen_name() {
-		return screen_name;
-	}
-	public void setScreen_name(String screen_name) {
-		this.screen_name = screen_name;
-	}
-	public String getCreated_at() {
-		return created_at;
-	}
-	public void setCreated_at(String created_at) {
-		this.created_at = created_at;
-	}
+	private String content;
+	private String name;
+	private String time;
 	
 }
