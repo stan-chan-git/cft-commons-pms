4443c130fc49429c91399e5f3a38b12f4325559d
tiffanychan <tiffanychencxt@gmail.com>
2014-04-22 09:15:56 +0800

    js文件格式修改

--- src/main/webapp/static/api/facebook.js
+++ src/main/webapp/static/api/facebook.js
@@ -3,40 +3,40 @@
  */
 function writeNewPost(){
 		
-			FB.init({
-				appId : '137410796429161',
-				status : true,
-				xfbml : true
-			});
-			
-			FB.getLoginStatus(function(response) {
-				if (response.status === 'connected') {
-					alert("已登录");
-					var uid = response.authResponse.userID;
-					var facebookToken = response.authResponse.accessToken;
-					postFBmsg(document.getElementById("msg").value,facebookToken);
-				} else {
-					FB.login(function (response){
-						
-						if (response.status === 'connected') {
-							alert("回调");
-							var facebookToken = response.authResponse.accessToken;
-							postFBmsg(document.getElementById("msg").value,facebookToken);
-						}
-					});
-				}
-			});
-	}
-	
-	function postFBmsg(msg,facebookToken){
-		FB.api('/me/feed', 'post', {
-			message : msg,
-			access_token : facebookToken
-		}, function(response) {
-			if (!response || response.error) {
-				alert('Error occured:'+response.error);
+		FB.init({
+			appId : '137410796429161',
+			status : true,
+			xfbml : true
+		});
+		
+		FB.getLoginStatus(function(response) {
+			if (response.status === 'connected') {
+				alert("已登录");
+				var uid = response.authResponse.userID;
+				var facebookToken = response.authResponse.accessToken;
+				postFBmsg(document.getElementById("msg").value,facebookToken);
 			} else {
-				alert('Post ID: ' + response.id);
+				FB.login(function (response){
+					
+					if (response.status === 'connected') {
+						alert("回调");
+						var facebookToken = response.authResponse.accessToken;
+						postFBmsg(document.getElementById("msg").value,facebookToken);
+					}
+				});
 			}
 		});
-	} 
+}
+	
+function postFBmsg(msg,facebookToken){
+	FB.api('/me/feed', 'post', {
+		message : msg,
+		access_token : facebookToken
+	}, function(response) {
+		if (!response || response.error) {
+			alert('Error occured:'+response.error);
+		} else {
+			alert('Post ID: ' + response.id);
+		}
+	});
+} 
