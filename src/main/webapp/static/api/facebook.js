6ca01c6cb5f007764b5ff314ab6f017669e1e06b
taurus <xxh0755@gmail.com>
2014-04-24 14:21:30 +0800

    屏蔽弹出框
    Signed-off-by: taurus <xxh0755@gmail.com>

--- src/main/webapp/static/api/facebook.js
+++ src/main/webapp/static/api/facebook.js
@@ -116,7 +116,7 @@ function postFBmsgpic(msg,facebookToken,callback){
  * 获取好友动态
  */
 function facebookFriendsDyn(callback){
-	alert("i come in facebook.js");
+	//alert("i come in facebook.js");
 	$.post("/pms/facebook/facebookFriendsDyn.do",
 		
 		   function(data){
