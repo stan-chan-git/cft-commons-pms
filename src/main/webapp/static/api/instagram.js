04b0058ddf4134107fb17cd4b4b57574fe4c3716
user <user@10.211.55.3>
2014-05-04 14:40:58 +0800

    写了一个返回全部照片的

--- src/main/webapp/static/api/instagram.js
+++ src/main/webapp/static/api/instagram.js
@@ -8,3 +8,13 @@ function instagramfriends(callback){
 			// 返回数据类型
 			"json");
 }
+//获取全部最新动态
+function instagramfriends(callback){
+	$.post("/pms/instagram/followFirend2.do",
+			// 回调函数
+			function(data){
+		 		callback(data); 
+			},
+			// 返回数据类型
+			"json");
+}
